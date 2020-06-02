ab4c95cf78f99f96ab4a05613bdd9ccfe16c2475
Justin Tam <tamtzekin@gmail.com>
2020-06-02 11:04:46 +1000

    Full win text added

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,6 +94,15 @@ function winCondition(doors) {
     return door.open === false
   }).length === 0;
   if (allDoorsUnlocked) {
-    winText.classList.remove("is-paused")
+    winText.innerHTML = "<p>You speak the four words into the receiver. The voice at the other end is silent, and for one dreadful moment you think you’ve gotten it wrong... then you hear a click. It comes from the glass door, the one that locked you in here at the start of all this. Is it open? Are you free? But what about the rewards the letter promised?</p>" +
+    "<p>The soft voice on the phone speaks at last. </p>" +
+    "<p>“Go outside and turn around.”</p>" +
+    "<p>Bewildered, you do as it says. The door opens easily and you make your way back to the main foyer. It’s dark and ghostly silent – but as you reach the entrance, the warm, familiar sight of the Harbour greets you. It seems somehow brighter than usual. You head out and walk until you’re standing in front of the Monumental Steps. There are few passers-by at this hour, all of whom seem to be peering up at something behind you. You turn around.</p>" +
+    "<p>Colours are swirling across the great white sails. Joyous reds, golds, aquas – all the colours of the painted animals you found – dancing over each other in a dazzling light projection. The person on the phone must be controlling it! The colours form words: those same words that made up the password. LOVE, BRAVERY, BROTHERHOOD, TRUTH. Then... one more.</p>" + 
+    "<p>CLIMB.</p>" +
+    "<p>You climb up the steps. Perched at the very top is a wooden box with a note attached.</p>" +
+    "<p>Remember that password, for it is the key to wisdom in much more than just opera. That, and this, is your reward. Do with it what makes you happiest, but do be careful handling it – it was crafted in 1791, for a very special premiere.</p>" +
+    "<p>You open the box. Inside sits an ornate carved wooden flute.</p>"
+    "<img src=\"https://www.sydneyoperahouse.com/content/dam/soh/digital/articles/escape-room/EscapeHouse-Room-Sails2-H.jpg\" alt=\"Illustration of Sydney Opera House with coloured sails\">"
   }
 }
