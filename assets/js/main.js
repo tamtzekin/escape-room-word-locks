9ff815189fbb378dd5d444fc76f41ea5fd92a10b
Justin Tam <tamtzekin@gmail.com>
2020-06-12 13:37:30 +1000

    Cleaned up indents

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,16 @@ const statusElement = document.getElementById('status');
 const winText = document.getElementById('wintext');
  
 class Door {
-  inputElement;
-  constructor(answer, inputElement, markerElement) {
-    this.answer = answer;
-    this.inputElement = inputElement;
-    this.markerElement = markerElement;
-    this.open = false;
+    inputElement;
+    constructor(answer, inputElement, markerElement) {
+        this.answer = answer;
+        this.inputElement = inputElement;
+        this.markerElement = markerElement;
+        this.open = false;
   }
   getName() {
-    return this.answer + ' ' + this.open + ' ';
-  }
+      return this.answer + ' ' + this.open + ' ';
+    }
 }
  
 // Build door data
@@ -22,86 +22,82 @@ const correctAnswers = ['LOVE', 'BRAVERY', 'BROTHERHOOD', 'TRUTH'];
 const markerIds = ['questiononemarker', 'questiontwomarker', 'questionthreemarker', 'questionfourmarker'];
 
 if (formIds.length != correctAnswers.length) {
-  throw new Error('# of answers doesnt match # of forms')
+    throw new Error('# of answers doesnt match # of forms')
 }
 const doors = [];
  
 for (let i = 0; i < correctAnswers.length; i++) {
-  doors.push(new Door(correctAnswers[i], document.getElementById(formIds[i]), document.getElementById(markerIds[i])));
+    doors.push(new Door(correctAnswers[i], document.getElementById(formIds[i]), document.getElementById(markerIds[i])));
 }
- 
-// now each door knows its answer, its form element, and its state of open/closed
- 
+  
 // Handle answer input
 form.addEventListener('submit', (e) => {
-  // guesses changes each submit, it doesn't need to exist as a global var
-  const guesses = [];
-  for (const door of doors) {
-    guesses.push(door.inputElement.value);
-  }
-  checkAnswers(doors, guesses);
-  winCondition(doors);
-  e.preventDefault();
+    // guesses changes each submit, it doesn't need to exist as a global var
+    const guesses = [];
+    for (const door of doors) {
+        guesses.push(door.inputElement.value);
+    }
+    checkAnswers(doors, guesses);
+    winCondition(doors);
+    e.preventDefault();
 });
  
 // functions should have input/output and not exist inside main thread flow generally
 function checkAnswers(doors, guesses) {
-  const messages = [];
-  let atLeastOneInWrongPosition = false;
-  for (let i = 0; i < doors.length; i++) {
-    const guess = guesses[i];
-    const correctAnswer = doors[i].answer;
-    if (guess === correctAnswer) {
-      doors[i].open = true;
-      doors[i].inputElement.disabled = true;
-      doors[i].markerElement.classList.remove('is-paused');
-      doors[i].markerElement.classList.remove('is-locked');
-      doors[i].markerElement.classList.add('is-open');
-      doors[i].get;
-      console.log(doors[i].markerElement);
-
-    } else if (correctAnswers.includes(guess)) {
-      atLeastOneInWrongPosition = true;
-
-    } else if (guess !== correctAnswer) {
-      doors[i].markerElement.classList.remove('is-paused');
-      doors[i].markerElement.classList.add('is-locked');
-      console.log(doors[i].markerElement);
+    const messages = [];
+    let atLeastOneInWrongPosition = false;
+    for (let i = 0; i < doors.length; i++) {
+        const guess = guesses[i];
+        const correctAnswer = doors[i].answer;
+        
+        if (guess === correctAnswer) {
+            doors[i].open = true;
+            doors[i].inputElement.disabled = true;
+            doors[i].markerElement.classList.remove('is-paused');
+            doors[i].markerElement.classList.remove('is-locked');
+            doors[i].markerElement.classList.add('is-open');
+            doors[i].get;
+            console.log(doors[i].markerElement);
+        } else if (correctAnswers.includes(guess)) {
+            atLeastOneInWrongPosition = true;
+        } else if (guess !== correctAnswer) {
+            doors[i].markerElement.classList.remove('is-paused');
+            doors[i].markerElement.classList.add('is-locked');
+            console.log(doors[i].markerElement);
+        }
     }
-
-  }
   // TODO: remove debug statement
   // console.log(JSON.stringify(doors));
-  const correctCount = doors.filter(door => door.open === true).length;
-  if (correctCount === 0) {
+const correctCount = doors.filter(door => door.open === true).length;
+if (correctCount === 0) {
     messages.push('That\'s not it.\n Did you remember to put them in order?\n Try again.')
-  } else if (correctCount > 0 && correctCount < 2) {
+} else if (correctCount > 0 && correctCount < 2) {
     messages.push('You\'re almost there.')
-  } else if (correctCount > 0 && correctCount < 3) {
+} else if (correctCount > 0 && correctCount < 3) {
     messages.push('Keep going.')
-  } else if (correctCount > 0 && correctCount < 4) {
+} else if (correctCount > 0 && correctCount < 4) {
     messages.push('Just one more to go.')
-  }
-  if (atLeastOneInWrongPosition) {
+}
+if (atLeastOneInWrongPosition) {
     messages.push('Have you tried that word in a different spot?')
-  }
-  statusElement.innerText = messages.join('\n')
+}
+statusElement.innerText = messages.join('\n')
 }
  
 function winCondition(doors) {
-  const allDoorsUnlocked = doors.filter(door => {
-    return door.open === false
-  }).length === 0;
-  if (allDoorsUnlocked) {
-    winText.innerHTML = '<p>You speak the four words into the receiver. The voice at the other end is silent, and for one dreadful moment you think you’ve gotten it wrong... then you hear a click. It comes from the glass door, the one that locked you in here at the start of all this. Is it open? Are you free? But what about the rewards the letter promised?</p>' +
-    '<p>The soft voice on the phone speaks at last. </p>' +
-    '<p>“Go outside and turn around.”</p>' +
-    '<p>Bewildered, you do as it says. The door opens easily and you make your way back to the main foyer. It’s dark and ghostly silent – but as you reach the entrance, the warm, familiar sight of the Harbour greets you. It seems somehow brighter than usual. You head out and walk until you’re standing in front of the Monumental Steps. There are few passers-by at this hour, all of whom seem to be peering up at something behind you. You turn around.</p>' +
-    '<p>Colours are swirling across the great white sails. Joyous reds, golds, aquas – all the colours of the painted animals you found – dancing over each other in a dazzling light projection. The person on the phone must be controlling it! The colours form words: those same words that made up the password. LOVE, BRAVERY, BROTHERHOOD, TRUTH. Then... one more.</p>' + 
-    '<p>CLIMB.</p>' +
-    '<p>You climb up the steps. Perched at the very top is a wooden box with a note attached.</p>' +
-    '<p>Remember that password, for it is the key to wisdom in much more than just opera. That, and this, is your reward. Do with it what makes you happiest, but do be careful handling it – it was crafted in 1791, for a very special premiere.</p>' +
-    '<p>You open the box. Inside sits an ornate carved wooden flute.</p>' +
-    '<img src=\'https://www.sydneyoperahouse.com/content/dam/soh/digital/articles/escape-room/EscapeHouse-Room-Sails2-H.jpg\' alt=\'Illustration of Sydney Opera House against starry night sky with sails coloured green, red, yellow and white\' width=\'400px\'>'
-  }
+    const allDoorsUnlocked = doors.filter(door => {
+        return door.open === false
+    }).length === 0;
+    if (allDoorsUnlocked) {
+        winText.innerHTML = '<p>You speak the four words into the receiver. The voice at the other end is silent, and for one dreadful moment you think you’ve gotten it wrong... then you hear a click. It comes from the glass door, the one that locked you in here at the start of all this. Is it open? Are you free? But what about the rewards the letter promised?</p>' +
+        '<p>The soft voice on the phone speaks at last. </p>' +
+        '<p>“Go outside and turn around.”</p>' +
+        '<p>Bewildered, you do as it says. The door opens easily and you make your way back to the main foyer. It’s dark and ghostly silent – but as you reach the entrance, the warm, familiar sight of the Harbour greets you. It seems somehow brighter than usual. You head out and walk until you’re standing in front of the Monumental Steps. There are few passers-by at this hour, all of whom seem to be peering up at something behind you. You turn around.</p>' +
+        '<p>Colours are swirling across the great white sails. Joyous reds, golds, aquas – all the colours of the painted animals you found – dancing over each other in a dazzling light projection. The person on the phone must be controlling it! The colours form words: those same words that made up the password. LOVE, BRAVERY, BROTHERHOOD, TRUTH. Then... one more.</p>' + 
+        '<p>CLIMB.</p>' +
+        '<p>You climb up the steps. Perched at the very top is a wooden box with a note attached.</p>' +
+        '<p>Remember that password, for it is the key to wisdom in much more than just opera. That, and this, is your reward. Do with it what makes you happiest, but do be careful handling it – it was crafted in 1791, for a very special premiere.</p>' +
+        '<p>You open the box. Inside sits an ornate carved wooden flute.</p>' +
+        '<img src=\'https://www.sydneyoperahouse.com/content/dam/soh/digital/articles/escape-room/EscapeHouse-Room-Sails2-H.jpg\' alt=\'Illustration of Sydney Opera House against starry night sky with sails coloured green, red, yellow and white\' width=\'400px\'>'
+    }
 }
